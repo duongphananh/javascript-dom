006e3606deb168c8094a65373767c46e1a769f6b
duongphananh <duongphananh@gmail.com>
2018-09-18 01:38:58 +0700

    style

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ console.log("My github console");
 $( document ).ready(function() {    
     document.getElementsByTagName("head")[0].insertAdjacentHTML(
         "beforeend",
-        "<link rel=\"stylesheet\" href=\"https://sdks.shopifycdn.com/polaris/latest/polaris.css\" />");
+        "<link rel=\"stylesheet\" href=\"https://raw.githubusercontent.com/duongphananh/javascript-dom/master/style.css\" />");
 
-    var html = "<button type='button' class='Polaris-Button'><span class='Polaris-Button__Content'><span>Add Jackyy</span></span></button>";
-    var floatbutton = "<button type='button' style='top:0;right:0;position:fixed;' class='Polaris-Button Polaris-Button--primary'><span class='Polaris-Button__Content'><span>Duong Ngu</span></span></button>";
+    var html = "<button class='btn'>Button</button>";
+    var floatbutton = "<button type='button' style='top:0;right:0;position:fixed;' class='btn btn--primary'>Sticky Button</button>";
 
     $('body').append(html);
     $('body').append(floatbutton);
-    //   <div style="height: 500px;"><button type="button" class="Polaris-Button"><span class="Polaris-Button__Content"><span>Open</span></span></button></div>
 });
